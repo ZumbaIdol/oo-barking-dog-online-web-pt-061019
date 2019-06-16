a96b22e9ebe38cf9fcbc4954ab69bc69b30c1909
David Foote <david.a.foote@gmail.com>
2019-06-16 05:31:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog
 
-def name=(dog_name)
-  @this_dogs_name = dog_name
-end
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
 
-def name
-  @this_dogs_name
-end
+  def name
+    @this_dogs_name
+  end
 
-def bark
-  puts "woof!"
-end
+  def bark
+    puts "woof!"
+  end
 
